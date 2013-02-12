f9201505036276c6403b3ba70199f7ae0585493d
Mike O'Malley <spuriousdata@gmail.com>
2013-02-12 05:03:25 -0500

    complete solr driver configuration

--- brushfire/core/settings.py
+++ brushfire/core/settings.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from brushfire.core.exceptions import BrushfireConfigException
+from django.utils.importlib import import_module
 
 conf = settings.get('BRUSHFIRE', None)
 if conf is None:
     raise BrushfireConfigException("`BRUSHFIRE` dict must be present in settings module")
 
+"""
 BRUSHFIRE = {
     'host': 'http://localhost:8080/solr',
     'cache': {
@@ -34,17 +36,49 @@ BRUSHFIRE = {
             'swap_cores_on_complete': ['collection1', 'collection2'],
         },
     },
+    'query': {
+        'fields': '*,score',
+        'lparams': "{!edismax qf='text^2 name^100' bf='name'}",
+    },
 }
+"""
 
 class Configuration(object):
     def __init__(self, c):
         self.c = c
         self.__set('host')
+        self.__set('query_core', 'cores.query', '') 
+        self.__set('default_handler', 'handlers.default', 'select') 
+        self.__set('default_lparams', 'query.lparams', '') 
+        self.set_query_cache()
 
-    def __set(self, property, dict_key=None, required=True):
+    def set_query_cache(self):
+        c = self.get('cache.method', False)
+        if c is None:
+            return
+        else:
+            if c not in ('file', 'django'):
+                self.query_cache = import_module(c)()
+            elif c == 'file':
+                self.query_cache = self.get('cache.path')
+            elif c == 'django':
+                from django.core.cache import get_cache
+                self.query_cache = get_cache(self.get('cache.which', False, 'default'))
+
+    def __set(self, property, dict_key=None, required=True, default=None):
         if dict_key is None:
             dict_key = property
+        value = None
+        try:
+            value = self.get(dict_key, required)
+        except BrushfireConfigException as e:
+            if default is not None:
+                value = default
+            else:
+                raise
+        setattr(self, property, value)
 
+    def get(self, dict_key, required=True, default=None)
         value = self.c
         try:
             keypath = dict_key.split('.')
@@ -54,12 +88,22 @@ class Configuration(object):
             if required:
                 setting_path = "BRUSHFIRE['" + "']['".join([x for x in dict_key.split('.')]) + "']"
                 raise BrushfireConfigException("%s is a required setting" % setting_path)
+            else:
+                return default
         else:
-            setattr(self, property, value)
+            return value
 
     def configure_solr(self, solr):
-        s = solr(conf)
-        return s
+        self.solr_conn = solr(
+            self.host,
+            self.query_core,
+            self.default_handler,
+            self.default_lparmas,
+            self.query_cache,
+        )
+        return self.solr_conn
+
+configuration = Configuration(conf)
 
 if __name__ == '__main__':
     c = Configuration(BRUSHFIRE)
--- brushfire/core/solrengine.py
+++ brushfire/core/solrengine.py
@@ -4,16 +4,13 @@ from django.utils.importlib import import_module
 from brushfire.core.driver import Solr
 from brushfire.core.settings import configuration as conf
 
-driver = settings.get("BRUSHFIRE_SOLR_DRIVER", '')
+driver = conf.get('driver', False, None)
 if driver:
     driver = import_module(driver)
 else:
     driver = Solr
 
-
-class SolrManager(object):
+class SolrEngine(object):
     def __init__(self):
         self.solr = conf.configure_solr(driver)
-
-
 
