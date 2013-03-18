86cb64aad8dcbbbe0511945201d2a42f9ec7e9e0
Mike O'Malley <spuriousdata@gmail.com>
2013-03-18 19:08:59 -0400

    flesh out filtering more

--- brushfire/core/query.py
+++ brushfire/core/query.py
@@ -1,53 +1,77 @@
 from brushfire import solr
+from brushfire.core.driver.solr import SolrQuery
 from brushfire.core.settings import configuration as conf
+from django.db.models.query import QuerySet
 from django.db.models import Q
+from django.db.models.sql.constants import LOOKUP_SEP, QUERY_TERMS
+from django.utils.tree import Node
 
 import logging
 logger = logging.getLogger('brushfire.core.query')
 
-class BrushfireQuerySet(object):
-    def __init__(self, model):
-        self.model = model
-        self.results = []
-        self.querystring = "*:*"
-        self._filters = []
-        self._sort = ""
+class SearchNode(Node):
+    AND = 'AND'
+    OR = 'OR'
+    default = AND
 
-    def __len__(self):
-        self.__run()
-        return len(self.results)
+    def as_query_string(self, query_fragment_callback):
+        result = []
+        for child in self.children:
+            if hasattr(child, 'as_query_string'):
+                result.append(child.as_query_string(query_fragment_callback))
+            else:
+                expression, value = child
+                field, filter_type = self.split_expression(expression)
+                result.append(query_fragment_callback(field, filter_type, value))
+        conn = ' %s ' % self.connector
+        query_string = conn.join(results)
+        if query_string:
+            if self.negated:
+                query_string = 'NOT (%s)' % query_string
+            elif len(self.children) != 1:
+                query_string = '(%s)' % query_string
+        return query_string
 
-    def __getitem__(self, key):
-        self.__run()
-        return self.results[key]
-
-    def filter(self, *args, **kwargs):
-        logger.debug("Adding filter %s, %s", str(args), str(kwargs))
-        self._filters.append(Q(*args, **kwargs))
-        return self
-
-    def all(self):
-        return self
-
-    def get(self, *args, **kwargs):
-        r = self.filter(*args, **kwargs)
-        if len(r) > 1:
-            raise self.model.MultipleResultsFound
-        elif len(r) == 0:
-            raise self.model.DoesNotExist
+    def split_expression(self, expression):
+        parts = expression.split(LOOKUP_SEP)
+        field = parts[0]
+        if len(pargs) == 1 or parts[-1] not in QUERY_TERMS:
+            filter_type = 'contains'
         else:
-            return r[0]
+            filter_type = parts.pop()
+        return (field, filter_type)
 
-    def order_by(self, *args):
-        for i in args:
-            i = "%s ASC" % i if not i.startswith('-') else "%s DESC" % i
-            self._sort += i
-        return self
+class SQ(Q, SearchNode):
+    pass
 
-    def __run(self):
-        results = solr.search(self.querystring).get('response', {})
+class BrushfireQuerySet(QuerySet):
+    def __init__(self, model=None, query=None, using=None):
+        super(BrushfireQuerySet, self).__init__(model, query, using)
+        self.query = query or SolrQuery("*:*")
+
+    def order_by(self, *fields):
+        assert self.query.can_filter(), \
+                "Cannot filter a query once a slice has been taken."
+        clone = self._clone()
+        clone.query.clear_ordering(force_empty=False)
+        clone.query.add_ordering(*fields)
+        return clone
+
+    def iterator(self):
+        results = solr.search(self.query.get_querystring()).get('response', {})
         for x in results['docs']:
             obj = self.model()
             for k,v in x.items():
                 setattr(obj, k, v)
-            self.results.append(obj)
+            yield obj
+
+    def _filter_or_exclude(self, negate, *args, **kwargs):
+        if args or kwargs:
+            assert self.query.can_filter(), \
+                    "Cannot filter a query once a slice has been taken."
+            clone = self._clone()
+        if negate:
+            clone.query.add_q(~SQ(*args, **kwargs))
+        else:
+            clone.query.add_q(SQ(*args, **kwargs))
+        return clone
