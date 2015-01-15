a4d893a1cd87702aa9fbf47928c7a1f1f78a9d9f
Mike O'Malley <spuriousdata@gmail.com>
2015-01-15 18:03:09 -0500

    Separate our model classes from django's
    * Change BrushfireModel not to inherit from Model
    * Change BrushfireModelBase not to inherit from ModelBase
    * Add functionality to BrushfireModel and BrushfireModelBase that was lost with
      the change in inheritance

--- brushfire/core/models.py
+++ brushfire/core/models.py
@@ -1,6 +1,11 @@
+import inspect
 from brushfire.core.query import BrushfireQuerySet
+from brushfire.core.exceptions import *
+from django.apps import apps
 from django.db import models
-from django.db.models.base import ModelBase, Model
+from django.db.models.base import subclass_exception
+from django.db.models.options import Options
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.utils import six
 
 import logging
@@ -96,18 +101,86 @@ class BrushfireManager(object):
         except KeyError:
             return getattr(self.get_query_set(), name)
 
-class BrushfireModelBase(ModelBase):
+class BrushfireModelBase(type):
     def __new__(cls, name, bases, attrs):
         new_class = super(BrushfireModelBase, cls).__new__(cls, name, bases, attrs)
-        setattr(new_class, '_default_manager', BrushfireManager(new_class))
-        setattr(new_class, '_base_manager', BrushfireManager(new_class))
+        parents = [b for b in bases if isinstance(b, BrushfireModelBase)]
+        module = attrs.pop('__module__')
+        attr_meta = attrs.pop('Meta', None)
+        if not attr_meta:
+            meta = getattr(new_class, 'Meta', None)
+        else:
+            meta = attr_meta
+            
+        app_config = apps.get_containing_app_config(module)
+        setattr(new_class, 'objects', BrushfireManager(new_class))
+        
+        if getattr(meta, 'app_label', None):
+            label = meta.app_label
+        elif app_config:
+            label = app_config.label
+        else:
+            label = '__NONE__'
+            
+        new_class.add_to_class('_meta', Options(meta, 
+                **{'app_label':label}))
+            
+        new_class.add_to_class(
+                'DoesNotExist',
+                subclass_exception(
+                    str('DoesNotExist'),
+                    tuple(x.DoesNotExist for x in parents 
+                        if hasattr(x, '_meta') and 
+                            not x._meta.abstract) or (ObjectDoesNotExist,),
+                    module,
+                    attached_to=new_class))
+        new_class.add_to_class(
+                'MultipleObjectsReturned',
+                subclass_exception(
+                    str('MultipleObjectsReturned'),
+                    tuple(x.DoesNotExist for x in parents 
+                        if hasattr(x, '_meta') and 
+                            not x._meta.abstract) or (MultipleObjectsReturned,),
+                    module,
+                    attached_to=new_class))
+        new_class._prepare()
+        # ModelBase calls this, not sure what it does or if we need it here. Need to investigate further.
+        #new_class._meta.apps.register_model(new_class._meta.app_label, new_class)
         return new_class
 
-class BrushfireModel(six.with_metaclass(BrushfireModelBase), Model):
+    def add_to_class(cls, name, value):
+        if not inspect.isclass(value) and hasattr(value, 'contribute_to_class'):
+            value.contribute_to_class(cls, name)
+        else:
+            setattr(cls, name, value)
+            
+    def _prepare(cls):
+        cls._meta._prepare(cls)
+            
+class BrushfireModel(six.with_metaclass(BrushfireModelBase)):
     _deferred = False
     score = IntegerField()
+    
+    # These aren't inherited for some reason? It breaks the new migration commands
     class Meta:
         abstract = True # Stop Django from creating a BrushfireModel table
         managed = False # Stop Django from creating tables for subclasses
+        
+    @classmethod
+    def check(cls, **kwargs):
+        return []
 
-
+    def _get_pk_val(self, meta=None):
+        if not meta:
+            meta = self._meta
+        return getattr(self, meta.pk.attname)
+    
+    def _set_pk_val(self, value):
+        return setattr(self, self._meta.pk.attname, value)
+    
+    pk = property(_get_pk_val, _set_pk_val)
+    
+    def __init__(self, *args, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+    
